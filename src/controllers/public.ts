d73895d5906b8582b44c5fc2d67167a8da6859fa
backmeupplz <backmeupplz@gmail.com>
2019-08-17 19:46:38 -0700

    move md to top

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -6,19 +6,19 @@ import { readFileSync } from 'fs'
 
 @Controller('/')
 export default class {
+  @Get('/md')
+  async docsMD(ctx: Context) {
+    try {
+      ctx.body = readFileSync(`${__dirname}/../../docs/README.md`, 'UTF-8')
+    } catch (err) {
+      ctx.status = 500
+    }
+  }
   @Get('/')
   async docs(ctx: Context) {
     try {
       const md = readFileSync(`${__dirname}/../../docs/README.md`, 'UTF-8')
       ctx.body = Markdown.markdown.toHTML(md, 'Maruku')
-    } catch (err) {
-      ctx.status = 500
-    }
-  }
-  @Get('/md')
-  async docsMD(ctx: Context) {
-    try {
-      ctx.body = readFileSync(`${__dirname}/../../docs/README.md`, 'UTF-8')
     } catch (err) {
       ctx.status = 500
     }
