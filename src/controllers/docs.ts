a2404c5f5b4482d114389f4368de2da9347369ae
backmeupplz <backmeupplz@gmail.com>
2019-08-17 19:53:17 -0700

    fix issues

--- src/controllers/docs.ts
+++ src/controllers/docs.ts
@@ -1,16 +1,14 @@
 // Dependencies
 import { Controller, Get } from 'koa-router-ts'
 import { Context } from 'koa'
-import Markdown = require('markdown')
 import { readFileSync } from 'fs'
 
-@Controller('/')
+@Controller('/md')
 export default class {
   @Get('/')
   docs(ctx: Context) {
     try {
-      const md = readFileSync(`${__dirname}/../../docs/README.md`, 'UTF-8')
-      ctx.body = Markdown.markdown.toHTML(md, 'Maruku')
+      ctx.body = readFileSync(`${__dirname}/../../docs/README.md`, 'UTF-8')
     } catch (err) {
       ctx.status = 500
     }
