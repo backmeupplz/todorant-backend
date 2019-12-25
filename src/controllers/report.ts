65debba83b1d7b74c382daca86fc56201d8fe740
backmeupplz <backmeupplz@gmail.com>
2019-12-25 08:54:51 -0800

    add frogs slayed to the report

--- src/controllers/report.ts
+++ src/controllers/report.ts
@@ -10,8 +10,12 @@ export default class {
   @Get('/', authenticate)
   async docs(ctx: Context) {
     const user = ctx.state.user as InstanceType<User>
-    let todos = await TodoModel.find({ user: user._id, completed: true })
-    todos = todos.filter(todo => !!todo.date)
+    const todos = await TodoModel.find({
+      user: user._id,
+      completed: true,
+      date: { $exists: true },
+    })
+    console.log(todos.length)
     const completedTodosMap = {} as { [index: string]: number }
     for (const todo of todos) {
       const key = `${todo.monthAndYear}-${todo.date}`
@@ -21,8 +25,29 @@ export default class {
         completedTodosMap[key] = 1
       }
     }
+    const frogs = await TodoModel.find({
+      user: user._id,
+      completed: true,
+      frog: true,
+      date: { $exists: true },
+    })
+    console.log(frogs.length)
+    const completedFrogsMap = {} as { [index: string]: number }
+    for (const todo of frogs) {
+      const key = `${todo.monthAndYear}-${todo.date}`
+      if (completedFrogsMap[key]) {
+        completedFrogsMap[key] = completedFrogsMap[key] + 1
+      } else {
+        completedFrogsMap[key] = 1
+      }
+    }
+    console.log({
+      completedTodosMap,
+      completedFrogsMap,
+    })
     ctx.body = {
       completedTodosMap,
+      completedFrogsMap,
     }
   }
 }
