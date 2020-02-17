eb3c59e4245128139974f0c9daee6d0fe56110a1
backmeupplz <backmeupplz@gmail.com>
2020-02-16 18:47:57 -0800

    add socket authorization

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -15,19 +15,29 @@ io.on('connection', socket => {
       const user = await getUserFromToken(token)
       socket.join(user._id)
       socket.emit('authorized')
+      setAuthorized(socket, true)
     } catch (err) {
-      socket.leaveAll()
+      logout(socket)
       await report(err)
     }
   })
   socket.on('logout', async () => {
-    try {
-      socket.leaveAll()
-    } catch (err) {
-      await report(err)
-    }
+    logout(socket)
   })
 })
+
+function logout(socket: SocketIO.Socket) {
+  socket.leaveAll()
+  setAuthorized(socket, false)
+}
+
+function setAuthorized(socket: SocketIO.Socket, authorized: boolean) {
+  ;(socket as any).authorized = authorized
+}
+
+function isAuthorized(socket: SocketIO.Socket) {
+  return !!(socket as any).authorized
+}
 
 server.listen(3000).on('listening', () => {
   console.log('Sockets are listening on 3000')
