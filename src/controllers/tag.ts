887a60f98010e8af238db39b226ec0d513b6a81e
backmeupplz <backmeupplz@gmail.com>
2020-04-15 07:13:51 -0700

    add edit tag

--- src/controllers/tag.ts
+++ src/controllers/tag.ts
@@ -1,7 +1,7 @@
 import { errors } from '../helpers/errors'
 import { TagModel } from '../models/tag'
 import { Context } from 'koa'
-import { Controller, Get, Delete } from 'koa-router-ts'
+import { Controller, Get, Delete, Put } from 'koa-router-ts'
 import { authenticate } from '../middlewares/authenticate'
 import { requestSync } from '../sockets'
 
@@ -29,6 +29,26 @@ export default class {
     }
     // Edit and save
     tag.deleted = true
+    await tag.save()
+    // Respond
+    ctx.status = 200
+    // Trigger sync
+    requestSync(ctx.state.user._id)
+  }
+
+  @Put('/:id', authenticate)
+  async put(ctx: Context) {
+    // Parameters
+    const id = ctx.params.id
+    const { color } = ctx.request.body
+    // Find todo
+    const tag = await TagModel.findById(id)
+    // Check ownership
+    if (!tag || tag.user.toString() !== ctx.state.user._id.toString()) {
+      return ctx.throw(404, errors.noTag)
+    }
+    // Edit and save
+    tag.color = color || null
     await tag.save()
     // Respond
     ctx.status = 200
