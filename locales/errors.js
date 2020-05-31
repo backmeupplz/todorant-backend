06dbbde4e3379b42850e81092dc9cb51fad941ea
backmeupplz <backmeupplz@gmail.com>
2020-05-31 06:57:12 -0700

    move errors

---
+++ locales/errors.js
@@ -0,0 +1,46 @@
+module.exports = {
+  noUser: {
+    en: 'No user found',
+    ru: 'Пользователь не найден',
+  },
+  noTodo: {
+    en: 'No todo found',
+    ru: 'Задача не найдена',
+  },
+  authentication: {
+    en: 'Authentication failed',
+    ru: 'Аутентификация провалилась',
+  },
+  noTokenProvided: {
+    en: 'No authentication token provided',
+    ru: 'Нет токена авторизации',
+  },
+  invalidFormat: {
+    en: 'Invalid format',
+    ru: 'Неправильный формат',
+  },
+  facebookAlreadyConnected: {
+    en: 'Facebook is already connected',
+    ru: 'Фейсбук уже подключен',
+  },
+  googleAlreadyConnected: {
+    en: 'Google is already connected',
+    ru: 'Гугл уже подключен',
+  },
+  telegramAlreadyConnected: {
+    en: 'Telegram is already connected',
+    ru: 'Телеграм уже подключен',
+  },
+  subscription: {
+    en: 'You have to buy the subscription',
+    ru: 'Вам нужно приобрести подписку',
+  },
+  noTag: {
+    en: 'No hashtag found',
+    ru: 'Хештег не найден',
+  },
+  wrongDecryptionPassword: {
+    en: 'Wrong decryption password',
+    ru: 'Неправильный пароль расшифровки',
+  },
+}
