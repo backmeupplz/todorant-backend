2d6cd5df400d21c11c76946b1b80d66e7351c228
backmeupplz <backmeupplz@gmail.com>
2020-06-01 12:55:23 -0700

    add italian to telegram bot

--- locales/errors.js
+++ locales/errors.js
@@ -1,46 +1,46 @@
 module.exports = {
   noUser: {
-    en: "No user found",
-    ru: "Пользователь не найден",
+    en: 'No user found',
+    ru: 'Пользователь не найден',
   },
   noTodo: {
-    en: "No todo found",
-    ru: "Задача не найдена",
+    en: 'No todo found',
+    ru: 'Задача не найдена',
   },
   authentication: {
-    en: "Authentication failed",
-    ru: "Аутентификация провалилась",
+    en: 'Authentication failed',
+    ru: 'Аутентификация провалилась',
   },
   noTokenProvided: {
-    en: "No authentication token provided",
-    ru: "Нет токена авторизации",
+    en: 'No authentication token provided',
+    ru: 'Нет токена авторизации',
   },
   invalidFormat: {
-    en: "Invalid format",
-    ru: "Неправильный формат",
+    en: 'Invalid format',
+    ru: 'Неправильный формат',
   },
   facebookAlreadyConnected: {
-    en: "Facebook is already connected",
-    ru: "Фейсбук уже подключен",
+    en: 'Facebook is already connected',
+    ru: 'Фейсбук уже подключен',
   },
   googleAlreadyConnected: {
-    en: "Google is already connected",
-    ru: "Гугл уже подключен",
+    en: 'Google is already connected',
+    ru: 'Гугл уже подключен',
   },
   telegramAlreadyConnected: {
-    en: "Telegram is already connected",
-    ru: "Телеграм уже подключен",
+    en: 'Telegram is already connected',
+    ru: 'Телеграм уже подключен',
   },
   subscription: {
-    en: "You have to buy the subscription",
-    ru: "Вам нужно приобрести подписку",
+    en: 'You have to buy the subscription',
+    ru: 'Вам нужно приобрести подписку',
   },
   noTag: {
-    en: "No hashtag found",
-    ru: "Хештег не найден",
+    en: 'No hashtag found',
+    ru: 'Хештег не найден',
   },
   wrongDecryptionPassword: {
-    en: "Wrong decryption password",
-    ru: "Неправильный пароль расшифровки",
+    en: 'Wrong decryption password',
+    ru: 'Неправильный пароль расшифровки',
   },
 }
