baddef2bb6de3417f58c40efcc6cb1a1d35e3583
upacyxou <upacyxou@tutanota.com>
2020-07-05 05:10:16 +0400

    fix

--- src/controllers/hero.ts
+++ src/controllers/hero.ts
@@ -1,31 +1,17 @@
-import { Controller, Post, Get } from 'koa-router-ts'
+import { Controller, Get } from 'koa-router-ts'
 import { Context } from 'koa'
-import { authenticate, getUserFromToken } from '../middlewares/authenticate'
-import { Hero, HeroModel, getOrCreateHero } from '../models/hero'
+import { authenticate } from '../middlewares/authenticate'
+import { getOrCreateHero } from '../models/hero'
 
 @Controller('/hero')
 export default class {
   @Get('/', authenticate)
   async get(ctx: Context) {
-    const user = await getUserFromToken(ctx.headers.token)
-    const hero = await getOrCreateHero(user)
-    ctx.status = 200
-    ctx.body = hero
+    ctx.body = await getPoints(ctx)
   }
-  @Post('/', authenticate)
-  async post(ctx: Context) {
-    const points = ctx.request.body.points
-    const user = await getUserFromToken(ctx.headers.token)
-    await HeroModel.findOneAndUpdate(
-      { user: user._id },
-      { $inc: { points: +points } }
-    )
-  }
-  @Post('/initialize', authenticate)
-  async initiinitializelize(ctx: Context) {
-    const points = ctx.request.body.points
-    const user = await getUserFromToken(ctx.headers.token)
-    await HeroModel.findOneAndUpdate({ user: user._id }, { points: points })
-    ctx.status = 200
-  }
+}
+
+export async function getPoints(ctx: Context) {
+  const hero = await getOrCreateHero(ctx.state.user._id)
+  return hero.points
 }
