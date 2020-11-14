86bd9fddeb2860750533cd186ffbdd80beef3080
backmeupplz <backmeupplz@gmail.com>
2020-11-14 11:05:46 -0800

    fix tests

--- src/__tests__/helpers/isTodoOld.ts
+++ src/__tests__/helpers/isTodoOld.ts
@@ -5,76 +5,76 @@ describe('isTodoOld helper', () => {
   it('should detect if todo is old with todo\'s "month" property < 10', async () => {
     const todo = {
       ...completeTodo,
-      monthAndYear: '05-2020',
+      monthAndYear: '2020-05',
       date: '21',
     }
 
-    expect(isTodoOld(todo, '05-2020-20', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '05-2020-22', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2020-05-22', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2020-05-20', '01:00')).toBe(false)
 
-    expect(isTodoOld(todo, '05-2019-22', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '05-2021-22', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2019-05-22', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2021-05-22', '01:00')).toBe(true)
 
-    expect(isTodoOld(todo, '04-2020-21', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '06-2020-21', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2020-04-21', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2020-06-21', '01:00')).toBe(true)
 
-    expect(isTodoOld(todo, '06-2019-21', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '04-2021-21', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2019-06-21', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2021-04-21', '01:00')).toBe(true)
   })
 
   it('should detect if todo is old with todo\'s "month" property >= 10', async () => {
     const todo = {
       ...completeTodo,
-      monthAndYear: '11-2020',
+      monthAndYear: '2020-11',
       date: '21',
     }
 
-    expect(isTodoOld(todo, '11-2020-20', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '11-2020-22', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2020-11-20', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2020-11-22', '01:00')).toBe(true)
 
-    expect(isTodoOld(todo, '11-2019-22', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '11-2021-22', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2019-11-22', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2021-11-22', '01:00')).toBe(true)
 
-    expect(isTodoOld(todo, '10-2020-21', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '12-2020-21', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2020-10-21', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2020-12-21', '01:00')).toBe(true)
 
-    expect(isTodoOld(todo, '12-2019-21', '01:00')).toBe(false)
-    expect(isTodoOld(todo, '10-2021-21', '01:00')).toBe(true)
+    expect(isTodoOld(todo, '2019-12-21', '01:00')).toBe(false)
+    expect(isTodoOld(todo, '2021-10-21', '01:00')).toBe(true)
   })
 
   it('should detect if todo is old without todo\'s "date" property', () => {
     const todoWithoutDate = {
       ...completeTodo,
-      monthAndYear: '05-2020',
+      monthAndYear: '2020-05',
       date: null,
     }
 
-    expect(isTodoOld(todoWithoutDate, '04-2020-20', '00:00')).toBe(false)
-    expect(isTodoOld(todoWithoutDate, '05-2020-20', '00:00')).toBe(true)
-    expect(isTodoOld(todoWithoutDate, '05-2019-20', '00:00')).toBe(false)
-    expect(isTodoOld(todoWithoutDate, '10-2020-20', '00:00')).toBe(true)
+    expect(isTodoOld(todoWithoutDate, '2020-04-20', '00:00')).toBe(false)
+    expect(isTodoOld(todoWithoutDate, '2020-05-20', '00:00')).toBe(true)
+    expect(isTodoOld(todoWithoutDate, '2019-05-20', '00:00')).toBe(false)
+    expect(isTodoOld(todoWithoutDate, '2020-10-20', '00:00')).toBe(true)
   })
 
-  it('should detect if todo is old with not default startTimeOfDay', async () => {
+  it('should detect if todo is old with non-default startTimeOfDay', async () => {
     const todo = {
       ...completeTodo,
-      monthAndYear: '05-2020',
+      monthAndYear: '2020-05',
       date: '21',
     }
 
-    expect(isTodoOld(todo, '05-2020-21', '03:00', '04:20')).toBe(false)
-    expect(isTodoOld(todo, '05-2020-21', '05:00', '04:20')).toBe(true)
+    expect(isTodoOld(todo, '2020-05-22', '03:00', '04:20')).toBe(false)
+    expect(isTodoOld(todo, '2020-05-22', '05:00', '04:20')).toBe(true)
 
     const todoWithoutDate = {
       ...completeTodo,
-      monthAndYear: '10-2020',
+      monthAndYear: '2020-10',
       date: null,
     }
 
-    expect(isTodoOld(todoWithoutDate, '10-2020-31', '03:00', '04:20')).toBe(
+    expect(isTodoOld(todoWithoutDate, '2020-10-01', '03:00', '04:20')).toBe(
       false
     )
-    expect(isTodoOld(todoWithoutDate, '10-2020-31', '05:00', '04:20')).toBe(
+    expect(isTodoOld(todoWithoutDate, '2020-10-01', '05:00', '04:20')).toBe(
       true
     )
   })
