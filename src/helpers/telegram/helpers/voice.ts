f91e3a161b90acb56513fdd5265bdf5a2dbd9482
backmeupplz <backmeupplz@gmail.com>
2021-06-30 14:48:03 -0700

    fix translations

--- src/helpers/telegram/helpers/voice.ts
+++ src/helpers/telegram/helpers/voice.ts
@@ -48,7 +48,7 @@ async function sendTranscription(ctx, url, witLanguage) {
 async function updateMessagewithError(ctx, msg) {
   try {
     // Get text
-    let text = ctx.i18n.t('error')
+    let text = ctx.i18n.t('error_recognition')
 
     // Edit message
     await ctx.telegram.editMessageText(
