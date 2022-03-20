9d7d7b7465e23c3aacbd66facee15c1302ed2a12
Bleb1k <viktor.masnikov17283940@gmail.com>
2022-03-21 00:27:28 +0900

    Added more tests to linkify test

--- src/__tests__/helpers/linkify.ts
+++ src/__tests__/helpers/linkify.ts
@@ -15,4 +15,46 @@ describe('Linkify helper', () => {
       })
     ).toStrictEqual(['#test1', '#тест2', '#TEST3', '#ТЕСТ4'])
   })
+
+  it('should find tag in sentence', async () => {
+    expect(
+      linkify.match('Здесь производится #teSтИnГ посреди сообщения')[0].url
+    ).toBe('#teSтИnГ')
+    expect(linkify.match('#teSтИnГ в начале сообщения')[0].url).toBe('#teSтИnГ')
+    expect(
+      linkify.match('В конце сообщения производится #teSтИnГ')[0].url
+    ).toBe('#teSтИnГ')
+  })
+
+  it('should find multiple tags in sentence', async () => {
+    expect(
+      linkify.match('Здесь обитают #Test1 и #Test2').map((cur) => {
+        return cur.url
+      })
+    ).toStrictEqual(['#Test1', '#Test2'])
+    expect(
+      linkify.match('Здесь обитают #Тест1 и #Тест2').map((cur) => {
+        return cur.url
+      })
+    ).toStrictEqual(['#Тест1', '#Тест2'])
+  })
+
+  it('should find multiple of the same tags in sentence', async () => {
+    expect(
+      linkify.match('#Test is a #Test').map((cur) => {
+        return [cur.url, cur.index]
+      })
+    ).toStrictEqual([
+      ['#Test', 0],
+      ['#Test', 11],
+    ])
+    expect(
+      linkify.match('#Тест это #Тест').map((cur) => {
+        return [cur.url, cur.index]
+      })
+    ).toStrictEqual([
+      ['#Тест', 0],
+      ['#Тест', 10],
+    ])
+  })
 })
