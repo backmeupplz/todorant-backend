f45cf6c61658c068adfcf2a05937e92ea9c76379
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-04 09:01:02 -0700

    extract credentials

--- src/helpers/googleSubscriptionValidator.ts
+++ src/helpers/googleSubscriptionValidator.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const Verifier = require('google-play-billing-validator')
 
-const googleCredentials = require('../../assets/api-4987639842126744234-562450-c85efe0aadfc.json')
+const googleCredentials = require('../../assets/api-4987639842126744234-562450-fa8629fc9dbd.json')
 
 export const googleSubscriptionValidator = new Verifier({
   email: googleCredentials.client_email,
