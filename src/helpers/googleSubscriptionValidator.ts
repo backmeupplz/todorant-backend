b57e7cde0f2c3b599ad46fc7ecc4ef25722616f0
ruslan zapevalov <upacyxou@gmail.com>
2022-04-11 22:23:16 +0400

    change google api file

--- src/helpers/googleSubscriptionValidator.ts
+++ src/helpers/googleSubscriptionValidator.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const Verifier = require('google-play-billing-validator')
 
-const googleCredentials = require('../../assets/api-4987639842126744234-562450-fa8629fc9dbd.json')
+const googleCredentials = require('../../assets/api-4987639842126744234-562450-67105810381f.json')
 
 export const googleSubscriptionValidator = new Verifier({
   email: googleCredentials.client_email,
