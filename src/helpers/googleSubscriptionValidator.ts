9277e1f37bdb829ba305333ee9fe0a12590bc36d
Ruslan Zapevalov <upacyxou@tutanota.com>
2022-04-13 01:45:39 +0400

    fix tests

--- src/helpers/googleSubscriptionValidator.ts
+++ src/helpers/googleSubscriptionValidator.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const Verifier = require('google-play-billing-validator')
 
-const googleCredentials = require('../../assets/api-4987639842126744234-562450-67105810381f.json')
+const googleCredentials = require('../../assets/api-4987639842126744234-562450-696121ef8828.json')
 
 export const googleSubscriptionValidator = new Verifier({
   email: googleCredentials.client_email,
