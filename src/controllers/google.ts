f29e4665db81cd0cbc97467184e2f895e2c29fa4
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-10-27 10:17:51 -0700

    Update google.ts

--- src/controllers/google.ts
+++ src/controllers/google.ts
@@ -41,73 +41,73 @@ export default class GoogleController {
 
   @Post('/notifications')
   async post(@Ctx() ctx: Context) {
-    try {
-      const id = ctx.request.header['x-goog-channel-id']
-      const user = await UserModel.findOne({
-        _id: id,
-      })
-      const credentials = user.settings.googleCalendarCredentials
-      const api = getGoogleCalendarApi(credentials)
-      const todorantCalendar = await getTodorantCalendar(api)
-      const events = await getGoogleEvents(api, todorantCalendar)
-      const deletedEvents = events.filter((event) => {
-        return event.status === 'cancelled'
-      })
-      const timedEvents = events.filter((event) => {
-        if (!event.start) {
-          return false
-        }
-        return event.start.dateTime
-      })
-      deletedEvents.forEach(async (event) => {
-        await TodoModel.findOneAndUpdate(
-          {
-            _id: event.id,
-            time: { $ne: null },
-          },
-          { completed: true }
-        )
-      })
-      const changedTodos = [] as Todo[]
-      timedEvents.forEach(async (event) => {
-        const eventDate = event.start.dateTime
-        const todo = await TodoModel.findOne({
-          _id: event.id,
-        })
-        if (!todo) {
-          return
-        }
-        const monthAndYearInEvent = eventDate.substr(0, 7)
-        const timeInEvent = eventDate.substr(11, 5)
-        const dateInEvent = eventDate.substr(8, 2)
-        let needsSaving = false
-        if (timeInEvent !== todo.time) {
-          todo.time = timeInEvent
-          needsSaving = true
-        }
-        if (dateInEvent !== todo.date) {
-          todo.date = dateInEvent
-          needsSaving = true
-        }
-        if (monthAndYearInEvent !== todo.monthAndYear) {
-          todo.monthAndYear = monthAndYearInEvent
-          needsSaving = true
-        }
-        if (needsSaving) {
-          todo.save()
-          changedTodos.push(todo)
-        }
-      })
-      // Fix order
-      await fixOrder(
-        user,
-        changedTodos.map((t) => getTitle(t))
-      )
-      // Trigger sync
-      requestSync(user._id)
-      ctx.status = 200
-    } catch (err) {
-      ctx.status = 500
-    }
+  //   try {
+  //     const id = ctx.request.header['x-goog-channel-id']
+  //     const user = await UserModel.findOne({
+  //       _id: id,
+  //     })
+  //     const credentials = user.settings.googleCalendarCredentials
+  //     const api = getGoogleCalendarApi(credentials)
+  //     const todorantCalendar = await getTodorantCalendar(api)
+  //     const events = await getGoogleEvents(api, todorantCalendar)
+  //     const deletedEvents = events.filter((event) => {
+  //       return event.status === 'cancelled'
+  //     })
+  //     const timedEvents = events.filter((event) => {
+  //       if (!event.start) {
+  //         return false
+  //       }
+  //       return event.start.dateTime
+  //     })
+  //     deletedEvents.forEach(async (event) => {
+  //       await TodoModel.findOneAndUpdate(
+  //         {
+  //           _id: event.id,
+  //           time: { $ne: null },
+  //         },
+  //         { completed: true }
+  //       )
+  //     })
+  //     const changedTodos = [] as Todo[]
+  //     timedEvents.forEach(async (event) => {
+  //       const eventDate = event.start.dateTime
+  //       const todo = await TodoModel.findOne({
+  //         _id: event.id,
+  //       })
+  //       if (!todo) {
+  //         return
+  //       }
+  //       const monthAndYearInEvent = eventDate.substr(0, 7)
+  //       const timeInEvent = eventDate.substr(11, 5)
+  //       const dateInEvent = eventDate.substr(8, 2)
+  //       let needsSaving = false
+  //       if (timeInEvent !== todo.time) {
+  //         todo.time = timeInEvent
+  //         needsSaving = true
+  //       }
+  //       if (dateInEvent !== todo.date) {
+  //         todo.date = dateInEvent
+  //         needsSaving = true
+  //       }
+  //       if (monthAndYearInEvent !== todo.monthAndYear) {
+  //         todo.monthAndYear = monthAndYearInEvent
+  //         needsSaving = true
+  //       }
+  //       if (needsSaving) {
+  //         todo.save()
+  //         changedTodos.push(todo)
+  //       }
+  //     })
+  //     // Fix order
+  //     await fixOrder(
+  //       user,
+  //       changedTodos.map((t) => getTitle(t))
+  //     )
+  //     // Trigger sync
+  //     requestSync(user._id)
+  //     ctx.status = 200
+  //   } catch (err) {
+  //     ctx.status = 500
+  //   }
   }
 }
